ecf0cfd85d19628bfe7a1bc0fe677a20c546e584
MateusKha <mateusgnascimento@outlook.com>
2020-09-20 17:22:00 -0300

    Adicionando componente header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>EU SOU O REI DO MUNDO TESTAAAA</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
