2caef3b0a126d6e5734c8091629a6ee97a0d414a
MateusKha <mateusgnascimento@outlook.com>
2020-09-21 00:34:01 -0300

    Configurando Netlify

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'https://sds1-kharazam.herokuapp.com/';
+const BASE_URL = 'https://sds1-kharazam.herokuapp.com';
 
 const Charts = () => {
 
